12b8b16c56dd29a90ec0a207e4d9387fdc9bb6d5
Apoorva-Ghodkhande <89741261+Apoorva-Ghodkhande@users.noreply.github.com>
2023-06-22 16:08:44 +0530

    Express commit 2

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi=require('joi');
 const express=require('express');
 const app=express();
 
@@ -25,20 +26,50 @@ app.get('/api/courses',(req, res)=> {
     res.send(courses);
     });
 
-    app.get('/api/courses/:id',(req, res)=> {
+
+app.get('/api/courses/:id',(req, res)=> {
         const course=courses.find(c => c.id===parseInt(req.params.id));
        if(!course) res.status(404).send('course with required id not found');
        res.send(course);
         });
 
-  app.post('/api/courses',(req,res)=>{
-   const course={
+
+app.post('/api/courses',(req,res)=>{
+
+//  const schema={
+//     name: Joi.string().min(3).required
+//  };
+//  const result=Joi.validate(req.body,schema);
+
+//  if(result.error){
+//     res.status(400).send(result.error.details[0].message);
+//  }
+   
+ const course={
     id:courses.length+1,
     name:req.body.name
    };
    courses.push(course);
    res.send(course);
   });
+
+
+  app.put('/api/courses/:id',(req, res)=> {
+    const course=courses.find(c => c.id===parseInt(req.params.id));
+   if(!course) res.status(404).send('course with required id not found');
+   console.log(course);
+    course.name=req.body.name;
+    res.send(course);
+    });
+
+    app.delete('/api/courses/:id',(req, res)=> {
+        const course=courses.find(c => c.id===parseInt(req.params.id));
+       if(!course) res.status(404).send('course with required id not found');
+    
+      courses.splice(course,1);
+       res.send(course);
+        });   
+
 
 const port=process.env.PORT || 3000;
 app.listen(port, console.log(`Executing port ${port}`));
