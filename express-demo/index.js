964db2619bae8060eb7662438137cbaf65fbf5eb
Apoorva-Ghodkhande <89741261+Apoorva-Ghodkhande@users.noreply.github.com>
2023-06-23 14:56:39 +0530

    Express commit 3

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,10 +1,20 @@
+const morgan = require('morgan');
 const Joi=require('joi');
+const logger=require('./logger');
 const express=require('express');
 const app=express();
 
 app.use(express.json());
+app.use(logger);
+app.use(morgan('tiny'));
 
-const courses=[{id:1, name:'course1'},{id:2, name:'course2'},{id:3, name:'course3'}];
+
+
+const courses=[
+    {id:1, name:'course1'},
+    {id:2, name:'course2'},
+    {id:3, name:'course3'}
+];
 
 app.get('/',(req, res)=> {
 res.send('Hello World!!!');
