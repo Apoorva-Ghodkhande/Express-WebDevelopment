56d3d104e62210a27239e824307954ebc5d3a85f
Apoorva-Ghodkhande <89741261+Apoorva-Ghodkhande@users.noreply.github.com>
2023-06-24 14:02:32 +0530

    Express commit 6

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,6 +1,27 @@
+//without Arrow function
+// console.log('Before');
+// getUser(1,function(user){
+// console.log('User',user);
+// });
+// console.log('After');
+
+
+// function getUser(id,callback){
+//     setTimeout(()=>{
+//         console.log('Reading a user from a database');
+//         callback({id:id, username:'apoorva'});
+//     }
+//     ,2000);
+// }
+
+//with Arrow function
+console.log('');
 console.log('Before');
-getUser(1,function(user){
+getUser(1,(user)=>{
 console.log('User',user);
+getRepositories(user.username,function(repoArray){
+console.log(repoArray);
+});
 });
 console.log('After');
 
@@ -9,6 +30,15 @@ function getUser(id,callback){
     setTimeout(()=>{
         console.log('Reading a user from a database');
         callback({id:id, username:'apoorva'});
+    }
+    ,2000);
+}
+
+
+function getRepositories(username,callback){
+    setTimeout(()=>{
+        console.log('Reading a user from a database');
+        callback(['repo1','repo2','repo3']);
     }
     ,2000);
 }
